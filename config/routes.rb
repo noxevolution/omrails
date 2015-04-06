2f32aca8728291a488b136bc12f556085cf5e78b
noxevolution <proloyd4@gmail.com>
2015-04-07 00:57:41 +0530

    change in Homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Omrails::Application.routes.draw do
-  root 'welcome#index'
+  get "welcomme/index"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
